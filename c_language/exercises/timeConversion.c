0167d5462009d33c9522955891619663c51ddfc8
abdallah samy fahmy <63059435+abdallah7656@users.noreply.github.com>
2023-04-24 02:18:07 +0200

    Add files via upload

---
+++ c_language/exercises/timeConversion.c
@@ -0,0 +1,128 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+char* timeConversion(char* s) {
+    // extract hours, minutes, seconds, and period from the input string
+    int hh, mm, ss;
+    char period[3];
+    sscanf(s, "%d:%d:%d%s", &hh, &mm, &ss, period);
+
+    // adjust hours based on period
+    if (strcmp(period, "PM") == 0 && hh != 12) {
+        hh += 12;
+    } else if (strcmp(period, "AM") == 0 && hh == 12) {
+        hh = 0;
+    }
+
+    // create a new string in 24-hour format and return it
+    char* result = malloc(9 * sizeof(char));
+    sprintf(result, "%02d:%02d:%02d", hh, mm, ss);
+    return result;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+if (fptr == NULL) {
+    fprintf(stderr, "Error opening output file\n");
+    exit(EXIT_FAILURE);
+}
+
+    char* s = readline();
+
+    char* result = timeConversion(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
